36519a247ab01ee232620885e70bba749a68b77c
Simon-Drohsen <simondrohsend@gmail.com>
2024-02-20 16:25:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <PageHome />
+  </div>
+</template>
+
+<script>
+import PageHome from '@/components/PageHome.vue'
+export default {
+  name: 'App',
+  components: {
+    PageHome
+  }
+}
+</script>
+
+<style>
+@import 'assets/style.css';
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import PageHome from '@/components/PageHome.vue'
+
+const forumApp = createApp(App)
+forumApp.mount('#app')
